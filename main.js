b0de4369762283a73e60341a72e14f7509b23c6b
Kota Hashihama <kotahashihama@gmail.com>
2022-03-13 12:30:40 +0900

    xoa het chua masaki

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-console.log('hello ru');
-console.log('hello sunny');
-console.log('hello kota');
-console.log('hello tomomi');
-console.log('hello kim quy');
 console.log('hello masaki');
-console.log('hello ryo');
