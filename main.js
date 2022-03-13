070cd9748d3dd11d3f2685e00e5bacbdc310979b
Kota Hashihama <kotahashihama@gmail.com>
2022-03-13 12:12:47 +0900

    thay doi ten gia dinh

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
+console.log('hello ru');
 console.log('hello sunny');
 console.log('hello kota');
+console.log('hello tomomi');
+console.log('hello kim quy');
+console.log('hello masaki');
+console.log('hello ryo');
